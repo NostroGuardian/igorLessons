c5ee772d787b743bfed9a0be8bdaed6e8639c5d4
nostroguardian <shmelivan5@yandex.ru>
2023-10-04 21:43:41 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('test');
+const taskArray = ['Ivan', 'Arthur', 'Igor', 'Yasha', 'Ilya', 'Voice'];
+
+const actionButton = document.querySelector('.actionButton');
+const usersWrapper = document.querySelector('.users');
+actionButton.addEventListener('click', showUsers);
+function showUsers() {
+  taskArray.sort().forEach((el, i) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+        <h1 class='card__name'>${i + 1}. ${el}</h1>
+    `;
+    usersWrapper.append(card);
+  });
+}
